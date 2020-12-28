d6ff6633d190f623bbaf3c72702f891530708bcf
ribeshbasnet <ribeshbasnet19.rb@gmail.com>
2020-12-28 14:06:11 +0545

    Error on Daatabase on 0017

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,31 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import {TypeOrmModule} from '@nestjs/typeorm'
 import { ResturantModule } from './resturant/resturant.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV  === "dev" ? ".env.dev" 
+        : ".env.test",
+        ignoreEnvFile: process.env.NODE_ENV === "prod",
+    }),
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: true,
+      logging: true
+    }),
     GraphQLModule.forRoot({
        autoSchemaFile: true,
     }),
     ResturantModule,
-    TypeOrmModule.forRoot({
-      type: "postgres",
-      host: "localhost",
-      port: 5432,
-      username: "postgres",
-      password: "12345",
-      database: "nep-eats",
-      synchronize: true,
-      logging: true
-    }),
   ],
   controllers: [],
   providers: [],
