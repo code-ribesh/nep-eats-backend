1c4d79e60ab566ec11084999ea5dc70f667d5d95
ribeshbasnet <ribeshbasnet19.rb@gmail.com>
2020-12-29 23:00:41 +0545

    added graphql types on my user entity and core entity

--- src/common/entities/core.entity.ts
+++ src/common/entities/core.entity.ts
@@ -1,14 +1,18 @@
+import { Field } from "@nestjs/graphql";
 import { CreateDateColumn, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
 
 export class CoreEntity {
-    @PrimaryGeneratedColumn()
+   @PrimaryGeneratedColumn()
+   @Field(type=> Number)
     id:number;
 
-    @CreateDateColumn()
+   @CreateDateColumn()
+   @Field(type=> Date)
     createdAt: Date;
     
     @UpdateDateColumn()
+    @Field(type=> Date)
     updatedAt: Date;
 
 }
